01e16f4de02baf8e384e6d35f05da8f5a99f31dc
developerujjal <ujjal.vision02@gmail.com>
2025-03-05 13:21:59 +0600

    add details page

--- src/components/HomPage/Products.jsx
+++ src/components/HomPage/Products.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ProductsCard from '../Cards/ProductsCard/ProductsCard';
+import dbConnect from '@/lib/dbConnect';
 
 
 
 const Products = async () => {
 
+    const collectionName = dbConnect('services');
+    const services = await collectionName.find({}).toArray();
+    // console.log(services)
 
     return (
         <div className="container mx-auto px-4 md:px-12 lg:px-24 py-4">
@@ -14,11 +19,11 @@ const Products = async () => {
                     <p className="font-normal text-[#737373] capitalize text-sm md:text-base">The majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {/* {
-                        services?.result?.map(service => {
-                            return <ProductCard key={service?._id} product={service}/>
+                    {
+                        services?.map(service => {
+                            return <ProductsCard key={service?._id} product={service} />
                         })
-                    } */}
+                    }
                 </div>
                 <div className="text-center mt-10">
                     <button type="button" className="text-[#FF3811] border border-[#FF3811] focus:ring-1 focus:outline-none focus:ring-[#FF3811] font-medium rounded text-sm px-4 py-2.5 text-center font-inter">More Products</button>
---
+++ src/lib/dbConnect.js
@@ -0,0 +1,60 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+export const DbCollectionObj = {
+    serviceCollection: "services"
+}
+
+export default function dbConnect(collectionName) {
+
+
+    const uri = process.env.MONGODB_URI;
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    return client.db(process.env.DB_NAME).collection(collectionName);
+}
+
+/* 
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.DB_NAME;
+
+// Create a cached MongoClient instance
+let cachedClient;
+let cachedDb;
+
+export default async function dbConnect(collectionName) {
+
+    if (cachedClient && cachedDb) {
+        // Use cached client and database if available
+        return cachedDb.collection(collectionName);
+    }
+
+    // Create a new client if none exists
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        },
+    });
+
+    try {
+
+        // Cache the client and database
+        cachedClient = client;
+        cachedDb = client.db(dbName);
+
+        return cachedDb.collection(collectionName);
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+        throw error;
+    }
+}
+
+
+ */
